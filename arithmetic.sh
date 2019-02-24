63ca9854a2d5ea995260c984f6a509aee01b7b61
Nishant Pandey <pnishant46@gmail.com>
2019-02-24 11:42:24 +0530

    arith bash

--- arithmetic.sh
+++ arithmetic.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 read a b
-let "s=$a+$b"
+let "s=$a+$b"                        #let
 echo $s
 
 let s++
 echo $s
+
+let s=5+4
+echo $s
+
+let "s = 5+4"
+echo $s
+
+s=$(( 5+4 ))                          #double paranthesis
+echo $s
+
+c=$((5+4))
+echo $c  #9
+
+c=$(( $c+4 ))          #c=$((c+4)) will also work
+echo $c    #13
+
+a='Hello World'
+echo ${#a}   #11
+
 
 
